20edd111728400356e8ddaa5bd6ec462c693de0b
smx.pusha <139338836+smxpusha@users.noreply.github.com>
2026-08-06 09:52:57 +0200

    ENH - redesign sky flappy glider

---
+++ frontend/src/features/games/sky-flappy/SkyFlappyGlider.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg
+    class="sky-flappy-glider"
+    viewBox="0 0 88 56"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+  >
+    <path class="sky-flappy-glider__trail" d="M24 21 2 28l22 7 10-7Z" />
+    <path class="sky-flappy-glider__lower-wing" d="m34 29 18 24 14-7-9-19Z" />
+    <path class="sky-flappy-glider__upper-wing" d="m31 25 21-22 20 8-15 17Z" />
+    <path class="sky-flappy-glider__body" d="M15 28 51 15l35 13-35 13Z" />
+    <path class="sky-flappy-glider__cockpit" d="m49 17 14 6-12 5-11-5Z" />
+    <path class="sky-flappy-glider__keel" d="m51 28 9 16-9 5-8-18Z" />
+  </svg>
+</template>
