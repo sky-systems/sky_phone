1364a064f7f2745a5f3325a7c818865a83d016c5
Dominik <dominique@wellisch-itservice.de>
2026-09-17 20:09:40 +0200

    FIX - correct gallery theme and block radio input while using phone

--- frontend/src/views/apps/GalleryApp.vue
+++ frontend/src/views/apps/GalleryApp.vue
@@ -1236,7 +1236,8 @@ onBeforeUnmount(() => {
       <template #right>
         <div
           v-if="selectionMode"
-          class="gallery-header-actions sky-ui-provider sky-ui-provider--dark"
+          class="gallery-header-actions sky-ui-provider"
+          :class="{ 'sky-ui-provider--dark': phone.isDarkMode }"
         >
           <SkyToolbarPane class="gallery-header-tool gallery-header-tool--text">
             <SkyButton
@@ -1250,7 +1251,8 @@ onBeforeUnmount(() => {
         </div>
         <div
           v-else
-          class="gallery-header-actions sky-ui-provider sky-ui-provider--dark"
+          class="gallery-header-actions sky-ui-provider"
+          :class="{ 'sky-ui-provider--dark': phone.isDarkMode }"
         >
           <SkyToolbarPane class="gallery-header-tool gallery-header-tool--icon">
             <SkyButton
@@ -1777,7 +1779,7 @@ onBeforeUnmount(() => {
   justify-content: center;
 }
 .gallery-selection-action :deep(.sky-button) {
-  color: #fff;
+  color: var(--sky-text);
 }
 .gallery-grid--fill {
   flex: 1;
